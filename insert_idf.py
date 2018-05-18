a7bfb473651b99b6c86511631932d3fb8e801d8f
esanche06 <ethands@uci.edu>
2018-05-18 12:20:48 -0700

    Added IDF Insert Program
    Inserts the proper values into the IDF table

---
+++ insert_idf.py
@@ -0,0 +1,35 @@
+import mysql.connector
+import bs4
+import re
+import json
+import math
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+#SELECT COUNT(DISTINCT(doc_id) from tokens; <-- N
+#SELECT word, COUNT(*) FROM tokens GROUP BY word; <-- rs
+#INSERT INTO idf VALUES(rs->word, log(N/rs->count(*))
+
+cnx = mysql.connector.connect(user='root', password='122BSQ', database="searchenginedb")
+
+num_docs = cnx.cursor()
+num_docs_str = "SELECT COUNT(DISTINCT(doc_id)) from tokens;"
+num_docs.execute(num_docs_str)
+
+N = num_docs.fetchall()[0][0]
+
+token_doc_count = cnx.cursor()
+token_doc_str = "SELECT word, COUNT(*) FROM tokens GROUP BY word;"
+token_doc_count.execute(token_doc_str)
+
+results = token_doc_count.fetchall()
+
+insert_into_idf = cnx.cursor()
+insert_into_idf_str = "INSERT INTO idf(word, counts) VALUES(%s, %s);"
+
+for result in results:
+    print(result[0] + " " + str(math.log(N/int(result[1]))))
+    insert_into_idf.execute(insert_into_idf_str, (result[0], str(math.log(N/int(result[1])))))
+
+cnx.commit()
+
